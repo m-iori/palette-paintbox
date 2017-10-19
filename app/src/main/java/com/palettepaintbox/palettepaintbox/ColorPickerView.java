ca30b37577d1bb983c484527ef87bfb4263fe493
Rachel Smith <rachel.smith@homeworksenergy.com>
2017-10-19 08:21:24 -0400

    make colorwheel bigger

--- app/src/main/java/com/palettepaintbox/palettepaintbox/ColorPickerView.java
+++ app/src/main/java/com/palettepaintbox/palettepaintbox/ColorPickerView.java
@@ -11,8 +11,7 @@ import android.view.View;
 
 public class ColorPickerView extends View {
 
-    private static final int CENTER_X = 100;
-    private static final int CENTER_RADIUS = 32;
+    private static final int CENTER_X = 450;
 
     private final int[] mColors;
     private Paint mPaint;
@@ -30,7 +29,7 @@ public class ColorPickerView extends View {
         mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaint.setShader(s);
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeWidth(32);
+        mPaint.setStrokeWidth(300);
 
         mCenterPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mCenterPaint.setColor(Color.GREEN);
@@ -39,29 +38,9 @@ public class ColorPickerView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        float r = CENTER_X - mPaint.getStrokeWidth()*0.5f;
-
+        float r = CENTER_X - mPaint.getStrokeWidth();
         canvas.translate(CENTER_X, CENTER_X);
-
         canvas.drawOval(new RectF(-r, -r, r, r), mPaint);
-        canvas.drawCircle(0, 0, CENTER_RADIUS, mCenterPaint);
-
-//        if (mTrackingCenter) {
-//            int c = mCenterPaint.getColor();
-//            mCenterPaint.setStyle(Paint.Style.STROKE);
-//
-//            if (mHighlightCenter) {
-//                mCenterPaint.setAlpha(0xFF);
-//            } else {
-//                mCenterPaint.setAlpha(0x80);
-//            }
-//            canvas.drawCircle(0, 0,
-//                    CENTER_RADIUS + mCenterPaint.getStrokeWidth(),
-//                    mCenterPaint);
-//
-//            mCenterPaint.setStyle(Paint.Style.FILL);
-//            mCenterPaint.setColor(c);
-//        }
     }
 
 }
