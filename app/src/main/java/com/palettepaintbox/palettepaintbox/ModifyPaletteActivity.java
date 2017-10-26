158fa09ccb453db2f6c33aaf009b6bad2b596a8a
Rachel Smith <rachel.smith@homeworksenergy.com>
2017-10-25 22:47:03 -0400

    Update hex when color clicked and add plus sign to add colors

--- app/src/main/java/com/palettepaintbox/palettepaintbox/ModifyPaletteActivity.java
+++ app/src/main/java/com/palettepaintbox/palettepaintbox/ModifyPaletteActivity.java
@@ -6,7 +6,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import java.util.ArrayList;
@@ -15,11 +17,12 @@ public class ModifyPaletteActivity extends AppCompatActivity {
 
     LinearLayout mLinearLayout;
     Button mSelectedColor;
+    private ArrayList<String> colors;
+    private ArrayList<Button> buttons;
 
     public interface OnColorChangedListener {
         void colorChanged(int color);
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,9 @@ public class ModifyPaletteActivity extends AppCompatActivity {
 
         OnColorChangedListener l = new OnColorChangedListener() {
             public void colorChanged(int color) {
+                String hexColor = String.format("#%06X", (0xFFFFFF & color));
+                EditText hexText = (EditText) findViewById(R.id.paletteColorInput);
+                hexText.setText(hexColor);
                 Drawable backgroundShape = ContextCompat.getDrawable(mLinearLayout.getContext(),R.drawable.color_circle);
                 backgroundShape.mutate().setColorFilter(color, PorterDuff.Mode.MULTIPLY);
                 mSelectedColor.setBackground(backgroundShape);
@@ -37,20 +43,56 @@ public class ModifyPaletteActivity extends AppCompatActivity {
 
         ColorPickerView colorPickerView= new ColorPickerView(layout.getContext(), l);
         layout.addView(colorPickerView);
-        ArrayList<String> paletteColors = new ArrayList<>();
-        paletteColors.add("FFFFFF");
-        Palette newPalette = new Palette(-1, null, paletteColors);
+
+        colors = new ArrayList<>();
+        buttons = new ArrayList<>();
+
+        colors.add("FFFFFF");
+        colors.add("FFFFFF");
 
         mLinearLayout = (LinearLayout) findViewById(R.id.palette_linear_layout);
-        Drawable backgroundShape = ContextCompat.getDrawable(mLinearLayout.getContext(),R.drawable.color_circle);
-        backgroundShape.mutate().setColorFilter(Color.parseColor("#ffffff"), PorterDuff.Mode.MULTIPLY);
-        Button colorButton = new Button(mLinearLayout.getContext());
-        colorButton.setBackground(backgroundShape);
+        addColorsToView();
 
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(120,120);
-        layoutParams.setMargins(5,5,5,5);
-        colorButton.setLayoutParams(layoutParams);
-        mLinearLayout.addView(colorButton);
-        mSelectedColor = colorButton;
+    }
+
+    private void addColorsToView(){
+        mLinearLayout.removeAllViews();
+        for(String color : this.colors) {
+            Drawable backgroundShape = ContextCompat.getDrawable(mLinearLayout.getContext(), R.drawable.color_circle);
+            String hexColor = "#" + color;
+            backgroundShape.mutate().setColorFilter(Color.parseColor(hexColor), PorterDuff.Mode.MULTIPLY);
+            Button colorButton = new Button(mLinearLayout.getContext());
+            colorButton.setBackground(backgroundShape);
+            colorButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mSelectedColor = (Button)v;
+                }
+            });
+
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(120, 120);
+            layoutParams.setMargins(5, 5, 5, 5);
+            colorButton.setLayoutParams(layoutParams);
+            mLinearLayout.addView(colorButton);
+            buttons.add(colorButton);
+        }
+        mSelectedColor = buttons.get(0);
+
+        if (colors.size() < 6) {
+            Drawable backgroundShape = ContextCompat.getDrawable(mLinearLayout.getContext(), R.drawable.ic_add_black_24dp);
+            Button addButton = new Button(mLinearLayout.getContext());
+            addButton.setBackground(backgroundShape);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(120, 120);
+            layoutParams.setMargins(5, 5, 5, 5);
+            addButton.setLayoutParams(layoutParams);
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    colors.add("FFFFFF");
+                    addColorsToView();
+                }
+            });
+            mLinearLayout.addView(addButton);
+        }
     }
 }
