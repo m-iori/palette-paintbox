ece7bb34f9bf51ffd6133c0511b32d9d14e12275
Rachel Smith <rachel.smith@homeworksenergy.com>
2017-11-08 23:26:08 -0500

    another unit test

---
+++ app/src/androidTest/java/com/palettepaintbox/palettepaintbox/ViewAllActivityTest.java
@@ -0,0 +1,29 @@
+package com.palettepaintbox.palettepaintbox;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertEquals;
+
+import android.support.v7.widget.Toolbar;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ViewAllActivityTest {
+    @Rule
+    public ActivityTestRule<ViewAllActivity> mActivityRule = new ActivityTestRule(ViewAllActivity.class);
+
+    /**
+     * Test that the title is correct.
+     */
+    @Test
+    public void lastItem_NotDisplayed() {
+        Toolbar mToolbar = (Toolbar)mActivityRule.getActivity().findViewById(R.id.toolbar);
+        assertEquals("Palette Paintbox", mToolbar.getTitle());
+    }
+
+}
